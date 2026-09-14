9c989d8366930a3f40f477fa6b4fd631e67e32ca
giantbug1 <rowanoud@gmail.com>
2026-09-14 12:25:53 +0200

    Update sketch.js

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,13 +1,27 @@
+function getrandomcolor1() {
+  return color(random(0,255),random(0,255),random(0,255))
+}
+
+function getrandomcar() {
+  return(random(0,3))
+}
+
 function setup() {
   createCanvas(800, 600);
+  noStroke();
+  dayC = color(150,220,255);
+  nightC = color(0,0,50);
+  sunriseC = color(220,150,200)
+ carColor1 = getrandomcolor1()
+ carColor2 = getrandomcolor1()
+ carvariant1 = getrandomcar()
+
 
   
 }
-
+ 
 
 let light = 1
-
-
 
   function keyPressed() {
  if (keyCode === ENTER) {
@@ -18,7 +32,7 @@ let light = 1
  }
 } 
 }
-
+ 
 let sun = 0;
 let sun1 = 0;
 sun1 =40
@@ -42,23 +56,56 @@ let leaf3 = 50;
 let leafC = 1;
 let leaf4 = 60;
 let leafD = 1;
-let time = 0;
-let day = 1;
-let night = 2;
-let angle = framecount * 0.01
+let time;
+let C;
+let night = 0;
+let speedLane1G = 13;
+let speedLane1O = 0;
+let speedLane1R = 7;
+let speedLane2G = 10;
+let speedLane2O = 0;
+let speedLane2R = 6;
+let Lane1Start = 80;
+let Lane2Start = 70;
+
+
 
 
 function draw() {
-  background(220)
 
-  fill(0)
-  rect(0,0,800,600)
+    if (car1 > 900) {
+      carColor = getrandomcolor()
+    }
+  time = (frameCount % 1000) / 1000;
+
+ 
+  //day night cycle
+  if (time < 0.25) {
+   
+    C = lerpColor(nightC, sunriseC, time / 0.25);
+    
+  } else if (time < 0.25) {
+   
+    C = lerpColor(sunriseC, dayC, (time - 0.25) / 0.25);
+
+  } else if (time < 0.75) {
+    
+    C = lerpColor(dayC, sunriseC, (time - 0.5) / 0.25);
+
+  } else {
+   
+    C = lerpColor(sunriseC, nightC, (time - 0.75) / 0.25);
+  } 
+
+  background(C);
+
   
-  fill(135,206,235)
-rect(0,0,999,600)
+
   
-  stroke(0)
  //sun
+  if (time >= 0.25 && time < 0.75) {
+    let sunTime = (time - 0.25) / 0.5;
+    let sunX = 30 + sunTime * 740;
   push()
  noStroke()
  if (sun1 > 100) {
@@ -76,33 +123,92 @@ rect(0,0,999,600)
  }
  sun2 = sun2 + (1 * a)
  sun = sun + 10
- translate(sun,0)/*
- fill(135,206,235)
-  rect(-500,0,999,600)*/
+ translate(sunX,0)
  fill(255,140,0,90)
  circle(100,100,sun1)
  fill(255,69,0,90)
  circle(100,100,sun2)
  fill(255,255,0)
  circle(100,100,50)
- 
- if (sun > 999) {
-  sun = -999
- }
  pop()
+    
+  }
+
+  //moon
+  if (time >= 0.75 || time < 0.25) {
+    let moonTime;
+
+    if (time >= 0.75) {
+      moonTime = (time - 0.75) / 0.5;
+    } else {
+      moonTime = (time + 0.25) / 0.5;
+    }
+
+    let moonX = 30 + moonTime * 740;
+
+    push()
+    translate(moonX,0,0)
+    fill(240);
+    circle(100, 100, 50);
+    fill(200)
+    circle(90,90,13);
+    circle(112,97,11);
+    circle(102,113,12);
+    circle(90,104,10)
+    pop()
+  }
+   if (time >= 0.75 || time < 0.25) {
+    fill(255)
+    push()
+    translate(0,0)
+    circle(random(0,800),random(0,500),5);
+    circle(random(0,800),random(0,500),5);
+    circle(random(0,800),random(0,500),5);
+    circle(random(0,800),random(0,500),5);
+    circle(random(0,800),random(0,500),5);
+    circle(random(0,800),random(0,500),5);
+    circle(random(0,800),random(0,500),5);
+    circle(random(0,800),random(0,500),5);
+    pop()
+   }
+
+  //tower
+  fill(112,128,144)
+  rect(20,100,130,600)
+  fill(176,196,222,50);
+    rect(30,150,40,40,5)
+    rect(30,210,40,40,5)
+    rect(30,280,40,40,5)
+    rect(30,340,40,40,5)
+    rect(30,400,40,40,5)
+    rect(95,150,40,40,5)
+    rect(95,210,40,40,5)
+    rect(95,280,40,40,5)
+    rect(95,340,40,40,5)
+    rect(95,400,40,40,5)
+  if (time >= 0.75 || time < 0.25) {
+    fill(255,239,213)
+    rect(30,150,40,40,5)
+    rect(30,210,40,40,5)
+    rect(30,280,40,40,5)
+    rect(30,340,40,40,5)
+    rect(30,400,40,40,5)
+    rect(95,150,40,40,5)
+    rect(95,210,40,40,5)
+    rect(95,280,40,40,5)
+    rect(95,340,40,40,5)
+    rect(95,400,40,40,5)
+  }
+  //mauntains
  push() 
- //bergen
-translate(0,100)
+ translate(0,100)
   fill(70)
   triangle(410,80, 190, 450, 650, 450)
 
-  fi
-  ll(105)
+  fill(105)
   triangle(650,230, 370, 450, 950, 450)
   triangle(270,180, 140, 450, 400, 450)
-
   pop()
-
   noStroke(0)
 
   //road
@@ -129,21 +235,21 @@ translate(0,100)
  circle(340,255,20)
  fill (100,10,0)
  circle(340,210,20)
- pop()
-//road stipes
-stroke(190)
-strokeWeight(5)
-let linelength = 35;
-let lingegap= 70;
-let linecount= 12;
-let roadmiddle= 467;
+  pop()
+ //road stipes
+ stroke(190)
+  strokeWeight(5)
+ let linelength = 35;
+ let lingegap= 70;
+ let linecount= 12;
+ let roadmiddle= 467;
 
-for(x=0; x < linecount; x++){
+ for(x=0; x < linecount; x++){
   let newx = (x*lingegap);
   line(newx,roadmiddle,newx+linelength,roadmiddle)
-}
+  }
 
-strokeWeight(1)
+ strokeWeight(1)
 
  stroke(1)
 
@@ -384,71 +490,85 @@ noStroke()
  }
  pop()
 
-push()
-translate(200,0)
+  push()
+
+
+
+ translate(200,0)
   if(light == 1) {
     fill (0,255,0)
     circle(340,355,20)
-    car1 = car1 + 12
-    car2 = car2 + 10
+    car1 = car1 + lerp(speedLane1G, speedLane1O, 0.05)
+    car2 = car2 + lerp(speedLane2G, speedLane2O, 0.05)
   
   }
 
   if(light == 2) {
     fill (255,165,0)
     circle(340,333,20)
-    car1 = car1 + 6
-    car2 = car2 + 5
+    car1 = car1 + lerp(speedLane1R, speedLane1G, 0.05)
+    car2 = car2 + lerp(speedLane2R, speedLane2G, 0.05)
   }
   
 
   if(light == 3) {
     fill (255,0,0)
     circle(340,310,20)
-    if(car1 < 340) {
-      car1 = car1 + 6
-    }
-    if(car2 < 340) {
-      car2 = car2 + 5
-    }
-    if(car1 > 380) {
+    if(car1 > Lane1Start + 10) {
       car1 = car1 + 12
+    } else if(car1 < Lane1Start) {
+      car1 = lerp(car1, Lane1Start, 0.05)
     }
-    if(car2 > 380) {
-      car2 = car2 + 10
+    if(car2 > Lane2Start + 10) {
+      car2 = car2 + 12
+    } else if(car2 < Lane2Start) {
+      car2 = lerp(car2, Lane2Start, 0.05)
     }
-    
+   
   }
   pop()
    
  noStroke()
  push()
  
+ 
+
  translate(car1,100)
  
- fill(255,0,0)
- quad(5,300,90,300,95,320,0,320,1)
- quad(0,320,95,320,95,345,0,345)
- quad(92,315,108,320,110,345,95,345,1)
+ 
+ fill(carColor1)
+ if(carvariant1 = 1) {
+ quad(345,295,430,295,435,320,340,320,1)
+ quad(340,320,435,320,435,345,340,345)
+ quad(432,315,448,320,440,345,435,345,1)
+ } else if (carvariant1 = 2) {
+ quad(360,300,420,300,435,320,340,320,1)
+ quad(340,320,435,320,435,345,340,345)
+ quad(432,315,450,320,450,345,435,345,1)
+ }
+
  fill(50)
- circle(20,345,29)
- circle(80,345,29)
+ circle(360,345,29)
+ circle(410,345,29)
  if (car1 > 900) {
-  car1 = -200
+  car1 = -500
+  carColor1 = getrandomcolor1()
+
  }
   pop()
 
   push()
  translate(car2,150)
- fill(255,255,255)
- quad(5,300,90,300,95,320,0,320,1)
- quad(0,320,95,320,95,345,0,345)
- quad(92,315,108,320,110,345,95,345,1)
+ fill(carColor2)
+  quad(360,300,420,300,435,320,340,320,1)
+ quad(340,320,435,320,435,345,340,345)
+ quad(432,315,450,320,450,345,435,345,1)
  fill(50)
- circle(20,345,29)
- circle(80,345,29)
+ circle(360,345,29)
+ circle(410,345,29)
  if (car2 > 900) {
-  car2 = -200
+  car2 = -500
+  carColor2 = getrandomcolor1()
   
  } 
 pop()
@@ -479,3 +599,4 @@ pop()
  circle(110,280,60)
   pop()
 }
+
